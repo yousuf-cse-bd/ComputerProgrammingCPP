5717fe722818da3099d5f2814b13936f20301813
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:58:13 +0600

    Sum of two real numbers

---
+++ Program2_9.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file Program2_9.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief sum of two real number
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    double a, b, sum;
+    a = 9.5;
+    b = 8.743;
+    sum = a + b;
+    cout<<sum<<endl;
+    printf("%lf\n", sum);
+    printf("%.2lf\n",sum);
+    return 0;
+}
