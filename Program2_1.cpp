4abe67cb3fb207e6484bbce2270d808e859a004b
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:43:47 +0600

    Summation of two variable

---
+++ Program2_1.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Program2_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Just summation of two variable
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 50;
+    int b = 60;
+    int sum = a + b;
+    cout<<"Sum is: "<<sum<<endl;
+
+    return 0;
+}
