426223d487a5e43365ab23b439bf49f45c5d98d3
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:51:33 +0600

    Just say output

---
+++ Program2_4.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file Program2_4.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Just output
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int x, y;
+    x = 1;
+    y = x;
+    x = 2;
+    cout<<"Y = "<<y<<endl;
+    return 0;
+}
