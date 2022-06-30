6e08b16b76c1460a6d090378eb578388f13139d4
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 00:02:26 +0600

    Character input program

---
+++ Program2_12.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Program2_12.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Character input program
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char ch;
+    cout<<"Enter the first letter of your name: ";
+    cin>>ch;
+    cout<<"The first letter of your name: "<<ch<<endl;
+    return 0;
+}
