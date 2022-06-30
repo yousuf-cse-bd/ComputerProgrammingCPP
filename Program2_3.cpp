1d1863d62ad74cb1c326c184cd771c59d940d2dd
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:50:34 +0600

    Just sum of two variable, declared and comma operator

---
+++ Program2_3.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Program2_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Just summation of two variable,  inline declaration
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a , b , sum;
+    a = 50, b = 60;
+    sum = a + b;
+    cout<<"Sum is: "<<sum<<endl;
+
+    return 0;
+}
