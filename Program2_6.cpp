9354558227ab32de576f3813d81c40b86c4879d6
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:54:23 +0600

    Data type int but value real-number->auto casting

---
+++ Program2_6.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Program2_6.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief variable int but value real number
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 50.45, b = 60, sum;
+    sum = a + b;
+    cout<<a<< " + "<<b<<" = "<<sum<<endl;
+    return 0;
+}
