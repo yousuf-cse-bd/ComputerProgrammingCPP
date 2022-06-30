69c1c87750199f22b187776b14cefcd54bb400e1
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 00:10:42 +0600

    Arithmetic operations, resulting different variable

---
+++ Program2_14iii.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file Program2_14iii.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Add, Sub, Mult, Div (Arithmetic Operation), resulting different variables
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    double num1, num2;
+    cout<<"Please enter a number here: ";
+    cin>>num1;
+    cout<<"Please eneter another number here: ";
+    cin>>num2;
+    double add, sub, mult, div;
+    add = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+    cout<<num1<<" + "<<num2<<" = "<<add<<endl;
+    cout<<num1<<" - "<<num2<<" = "<<sub<<endl;
+    cout<<num1<<" x "<<num2<<" = "<<mult<<endl;
+    cout<<num1<<" / "<<num2<<" = "<<div<<endl;
+    return 0;
+}
