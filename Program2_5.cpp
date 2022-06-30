42a3500c1abd385dfbb150347fc52ee6b7752409
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:52:36 +0600

    Nice output like equation

---
+++ Program2_5.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Program2_5.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Nice output like equation
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 50, b = 60, sum;
+    sum = a + b;
+    cout<<a<< " + "<<b<<" = "<<sum<<endl;
+    return 0;
+}
