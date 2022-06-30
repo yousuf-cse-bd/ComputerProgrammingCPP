486604f24b9899987c16081c46a5fdf6d66f9489
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:42:47 +0600

    About my country

---
+++ Program1_2.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Program1_2.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Say your country
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout<<"I love my country, Bangladesh."<<endl;
+    return 0;
+}
