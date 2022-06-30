7d178150d7e94613699c15e859ab3797fbc75bfc
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-30 23:27:57 +0600

    First program just print something

---
+++ Program1_1.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Program1_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief First programming just print something
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout<<"Md. Yousuf Ali"<<endl;
+    return 0;
+}
