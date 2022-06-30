2777a703c7841d388d55f130bc7fe10c11a5e730
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 00:16:35 +0600

    This is comment showing program

---
+++ Program2_16.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file Program2_16.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief This is comment showing program
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    // Test Program - comment: sinle line
+    printf("Hello ");
+    /*
+        We have printed Hello,
+        now we shall print World.
+        Note that this is a multiple-line comment*/
+    cout<<"World"<<endl;
+    return 0;
+}
