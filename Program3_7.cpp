caa8ac95a88958f334e46c01532f3c6a2242a028
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 08:36:36 +0600

    IF vs IF

---
+++ Program3_7.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Program3_6.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief if vs if
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n = 10;
+    if(n < 30){
+        cout<<n<<" is less than 30."<<endl;
+    }
+    if(n < 50){
+        cout<<n<<" is less than 50."<<endl;
+    }
+    return 0;
+}
