9618ee1e543913bc3c36a7e007f810c80281425e
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 17:31:33 +0600

    Use of logical OR

---
+++ Program3_11.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Program3_11.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Logical OR application
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int num = 50;
+    if(num >= 1 || num <= 10){
+        cout<<"Yes"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
