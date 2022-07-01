5293c38dc62297eff53061e272f1cfd86973239c
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 18:03:51 +0600

    Infinity while loop

---
+++ Program4_3.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file Program4_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief infinty using while loop
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n = 1;
+    while (n <= 10){
+        cout<<n<<endl;
+    }
+    n++;
+    
+    return 0;
+}
