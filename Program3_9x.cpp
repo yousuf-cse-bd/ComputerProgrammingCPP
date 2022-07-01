cbc23564264e373afdca9f05d9766f449666d3b7
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 17:19:27 +0600

    Uppercase-Lowercase

---
+++ Program3_9x.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file Program3_9x.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Uppercase || Lowercase
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char letter = 'u';
+    if(letter >= 'A' && letter <= 'Z'){
+        cout<<"Upper Case"<<endl;
+    }
+    if(letter >= 'a' && letter <= 'z'){
+        cout<<"Lower Case"<<endl;
+    }
+
+    // if('A' < 'a'){
+    //     cout<<"a is greater than A?"<<endl;
+    // }else{
+    //     cout<<"A is greater than a?"<<endl;
+    // }
+
+    return 0;
+}
