b8e48f789bc8c0dc85328702cecb4b9683d228e7
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 17:59:33 +0600

    1 to 10 print using while-loop

---
+++ Program4_2.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Program4_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Multiple line print using while loop
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n = 1;
+    while (n <= 10){
+        cout<<n++<<endl;
+    }
+    
+    return 0;
+}
