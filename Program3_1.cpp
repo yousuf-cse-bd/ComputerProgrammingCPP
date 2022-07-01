1936355d36e507446968dc098dfaae7f9c0334e0
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 07:57:18 +0600

    Postivie and Negativie

---
+++ Program3_1.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file Program3_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Find positive and negative number
+ * @version 0.1
+ * @date 2022-07-01
+ * @since Friday; 7.49AM
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n = 10;
+    if(n >= 0){
+        cout<<"The number is positive."<<endl;
+    }else{
+        cout<<"The number is negative."<<endl;
+    }
+    return 0;
+}
