46b4cb182ea5c089a2e27ae9911ee3bf95904ecc
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 17:55:52 +0600

    Multiple line print

---
+++ Program4_1.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file Program4_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Multiple line print not using loop
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout<<"1\n2\n3\n4\n5\n6\n7\n8\n9\n10"<<endl;
+    return 0;
+}
