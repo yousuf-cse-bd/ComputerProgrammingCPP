431ee212971e485eb183595bdf5ce878f75ae795
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 16:57:06 +0600

    Find Even-Odd without modulus operator

---
+++ Program3_9EvenOdd.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file Program3_9EvenOdd.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Odd-even number
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int number, divResult, remainder;
+    number = 6;
+    divResult = number / 2;
+    remainder = number - (divResult * 2);
+    if(remainder == 0){
+        cout<<"The number is even."<<endl;
+    }
+    else{
+        cout<<"The number is odd."<<endl;
+    }
+    return 0;
+}
