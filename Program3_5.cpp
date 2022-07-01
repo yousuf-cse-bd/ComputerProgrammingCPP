637fd9cce8df4bb14351d7f6accadaebb9e5fb50
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-01 08:18:26 +0600

    Only IF condition

---
+++ Program3_5.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Program3_5.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Single if condition
+ * @version 0.1
+ * @date 2022-07-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int number  = 12;
+    if(number > 10){
+        cout<<"The number is greater than ten'10'."<<endl;
+    }
+    return 0;
+}
