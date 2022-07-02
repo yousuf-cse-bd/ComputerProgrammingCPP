37dba9977f839216deb217b6ef8f4de1715672e8
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-02 21:26:17 +0600

    Basic Array

---
+++ Program6_2.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Program6_2.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Basic Array
+ * @version 0.1
+ * @date 2022-07-02
+ * @since Saturday, 9.24PM
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int arr[5] = {10, 20, 30, 40, 50};
+    cout<<"First element: "<<arr[0]<<endl;
+    cout<<"Third element: "<<arr[2]<<endl;
+    return 0;
+}
