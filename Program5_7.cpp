1357c4694c5f123564a115fdc14ef643d1ec4c7d
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-02 09:47:44 +0600

    Celsius to Fahreinheit

---
+++ Program5_7.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Program5_7.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief F = (c * 1.8) + 32;
+ * @version 0.1
+ * @date 2022-07-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    double f,c;
+    cout<<"Enter Celsius value here: ";
+    cin>>c;
+    f = (c * 1.8) + 32;
+    cout<<"Fahreinheit: "<<f<<" in Degree"<<endl;
+    return 0;
+}
