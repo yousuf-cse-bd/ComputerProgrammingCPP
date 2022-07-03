98d930cd296368dc1b3d681b6f417fd883dab600
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-03 09:31:41 +0600

    Just Sum

---
+++ Program7_1.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file Program7_1cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Just Sum Real Values
+ * @version 0.1
+ * @date 2022-07-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    float a, b, c;
+    a = b = 2.6;
+    c = a + b;
+    cout<<c<<endl;
+    return 0;
+}
