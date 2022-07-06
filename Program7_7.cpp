e79095fd5769c322f7783a2e07a2ffced4166ca5
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-06 17:13:53 +0600

    Find max element from array

---
+++ Program7_7.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file Program7_7.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Find max value from array using user defined function
+ * @version 0.1
+ * @date 2022-07-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int findMaxElement(int A[], unsigned int size){
+    int maxElement = A[0];
+    for(unsigned int i = 1;i<size; i++){
+        if(A[i] > maxElement){
+            maxElement = A[i];
+        }
+    }
+    return maxElement;
+}
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n;
+    cout<<"Enter nth value here: ";
+    cin>>n;
+
+    unsigned int size = (n > 0)? n : (-1)*n;
+    int A[size];
+    for(unsigned int i = 0; i<size; i++){
+        cout<<"Index: ["<<i<<"] = Element = ";
+        cin>>A[i];
+    }
+    cout<<"The Max Element: "<<findMaxElement(A, size)<<endl;
+    return 0;
+}
