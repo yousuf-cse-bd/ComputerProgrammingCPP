386feb45f6ce6e931cea152aee921dd0734459e4
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-07 09:51:02 +0600

    First string program

---
+++ Program9_1.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Program9_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief First string program
+ * @version 0.1
+ * @date 2022-07-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char country[] = {'B', 'A', 'N', 'G', 'L', 'A', 'D', 'E', 'S', 'H', '\0'};
+
+    cout<<country<<endl;
+    return 0;
+}
