cebff2fd72240834fb66342df83839be12b677cc
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-07 10:29:35 +0600

    Null char into char array

---
+++ Program9_3.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Program9_3.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Second string program
+ * @version 0.1
+ * @date 2022-07-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char country[] = {'B', 'A', 'N', 'G', 'L', 'A', 'D', 'E', 'S', 'H', '\0', 'I', 'S',
+    ' ', 'M', 'Y', '\0'};
+
+    cout<<country<<" Country."<<endl;
+    return 0;
+}
