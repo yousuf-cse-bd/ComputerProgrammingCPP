9d7b611a63e490f26308b65c3c955b058bbb7892
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-09 23:25:18 +0600

    Find prime number using smart 'sqrt()'

---
+++ Program10_2.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file Program10_2.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Find prime number using sqrt()
+ * @version 0.1
+ * @date 2022-07-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n < 2){
+        return 0;
+    }
+    if(n == 2){
+        return 1;
+    }
+    if(n%2 == 0){
+        return 0;
+    }
+    for(unsigned int i = 3; i * i <=n; i++){
+        if(n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n;
+    while(true){
+        printf("Please, Enter a number (Enter 0 to EXIT):?");
+        cin>>n;
+        if(n == 0){
+            break;
+        }
+        if(isPrime(n)){
+            printf("\t%c%c%c ", 1,2,3);
+            cout<<n<<" is a prime number.";
+            printf(" %c%c%c\n",3,1,2);
+        }else{
+            printf("\t%c%c%c ", 4,5,6);
+            cout<<n<<" is a not prime number.";
+            printf(" %c%c%c\n", 6,5,4);
+        }
+    }
+    return 0;
+}
